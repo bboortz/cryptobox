fdc27e6e8f169d373bbde0281ec3653291c78520
bboortz <benjamin.boortz@mailbox.org>
2016-05-11 20:10:13 +0000

    lib.config.appconfig added /BB

---
+++ lib/config/appconfig.py
@@ -0,0 +1,33 @@
+import os
+from flaskconfig import ProductionFlaskConfig, DevelopmentFlaskConfig, TestingFlaskConfig
+
+class AppConfig(object):
+    IP = os.getenv('IP', '0.0.0.0')
+    PORT = int( os.getenv('PORT', 8080) )
+    APPNAME = "test"
+    APPVERSION = "0.1"
+    ENV = "PROD"
+    FLASKCONFIG = ProductionFlaskConfig
+    
+    @staticmethod
+    def create_instance( env=os.getenv('ENV', 'PROD') ):
+        result = ProductionAppConfig()
+        
+        if env == 'DEV':
+            result = DevelopmentAppConfig()
+        elif env == 'TEST':
+            result = TestingAppConfig()
+            
+        return result
+
+class ProductionAppConfig(AppConfig):
+    ENV = "PROD"
+    FLASKCONFIG = ProductionFlaskConfig
+
+class DevelopmentAppConfig(AppConfig):
+    ENV = "DEV"
+    FLASKCONFIG = DevelopmentFlaskConfig
+
+class TestingAppConfig(AppConfig):
+    ENV = "TEST"
+    FLASKCONFIG = DevelopmentFlaskConfig
