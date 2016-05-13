9dd484fb05327c5e6bc03a39047ba237070e44bb
bboortz <benjamin.boortz@mailbox.org>
2016-05-13 03:46:24 +0000

    using global variables /BB

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,11 @@ from nose.tools import assert_not_equal
 
 
 class TestApp(object):
+
     @classmethod
     def setup_class(klass):
         """This method is run once for each class before any tests are run"""
+        
 
     @classmethod
     def teardown_class(klass):
@@ -16,31 +18,28 @@ class TestApp(object):
 
     def setUp(self):
         """This method is run once before _each_ test method is executed"""
+        self.test_app = app.test_client()
 
     def teardown(self):
         """This method is run once after _each_ test method is executed"""
 
     def test_slash(self):
-        test_app = app.test_client()
-        rv = test_app.get('/')
+        rv = self.test_app.get('/')
         assert_equal(rv.status_code, 404)
         assert_not_equal(rv.status_code, 201)
 
     def test_alive(self):
-        test_app = app.test_client()
-        rv = test_app.get('/alive')
+        rv = self.test_app.get('/alive')
         assert_equal(rv.status_code, 200)
         assert_not_equal(rv.status_code, 201)
 
     def test_api(self):
-        test_app = app.test_client()
-        rv = test_app.get('/api')
+        rv = self.test_app.get('/api')
         assert_equal(rv.status_code, 200)
         assert_not_equal(rv.status_code, 201)
 
     def test_config(self):
-        test_app = app.test_client()
-        rv = test_app.get('/api/config')
+        rv = self.test_app.get('/api/config')
         assert_equal(rv.status_code, 200)
         assert_not_equal(rv.status_code, 201)
 
@@ -51,7 +50,6 @@ class TestApp(object):
         assert_not_equal(rv.status_code, 201)
 
     def test_file(self):
-        test_app = app.test_client()
-        rv = test_app.get('/api/file')
+        rv = self.test_app.get('/api/file')
         assert_equal(rv.status_code, 200)
         assert_not_equal(rv.status_code, 201)
