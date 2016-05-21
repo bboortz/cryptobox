1e3a08da051f1dee6d351ddcdbc7dced157fd7db
bboortz <benjamin.boortz@mailbox.org>
2016-05-21 17:32:09 +0000

    wtf forms added /BB

--- lib/flaskconfig.py
+++ lib/flaskconfig.py
@@ -1,3 +1,4 @@
+import os
 from flask import __version__ as FLASK_VERSION
 from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
 
@@ -7,6 +8,7 @@ class FlaskConfig(object):
     TESTING = False
     FLASK_VERSION = FLASK_VERSION
     FLASK_BOOTSTRAP_VERSION = FLASK_BOOTSTRAP_VERSION
+    SECRET_KEY = SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'eoki99)u9uJ)J)J9jhgu5hg49)jjjJJKKK0Ij9jf')
     
     #DATABASE_URI = 'sqlite://:memory:'
     BOOTSTRAP_USE_MINIFIED = True
@@ -20,6 +22,8 @@ class ProductionFlaskConfig(FlaskConfig):
 
 class DevelopmentFlaskConfig(FlaskConfig):
     DEBUG = True
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'secret_dev_key_24239834958495')
 
 class TestingFlaskConfig(FlaskConfig):
     TESTING = True
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'secret_test_key_kogk94ik9gi54')
