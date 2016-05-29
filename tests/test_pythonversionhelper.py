50396108bf0765ba47262bcee6f3c71fc14253bf
bboortz <benjamin.boortz@mailbox.org>
2016-05-29 11:21:53 +0000

    str_to_bytes introduced for better python 2/3 compability /BB

--- tests/test_pythonversionhelper.py
+++ tests/test_pythonversionhelper.py
@@ -31,4 +31,31 @@ class TestPythonVersionHelper(object):
         assert_equal( isinstance_of_string(b"asdasdsa"), True)
         
         assert_not_equal( isinstance_of_string(123434), True)
+    
+    def test_str_to_bytes(self):
+        str = "this is a test string"
+        ustr = u"this is a test string type unicode"
+        bstr = b"this is a test string type byte"
+        
+        assert_equal( isinstance_of_string( str ), True)
+        assert_equal( isinstance_of_string( ustr ), True)
+        assert_equal( isinstance_of_string( bstr ), True)
+        
+        str_byte = str_to_bytes(str)
+        ustr_byte = str_to_bytes(ustr)
+        bstr_byte = str_to_bytes(bstr)
+        
+        print type (str_byte)
+        print type (ustr_byte)
+        print type (bstr_byte)
+        print type ( str_to_bytes("  ") )
+        print str.encode()
+        assert_equal( isinstance_of_string("  "), False)
+        assert_equal( isinstance_of_string("123131"), True)
+        assert_equal( isinstance_of_string(u"  "), True)
+        assert_equal( isinstance_of_string(u"aasds"), True)
+        assert_equal( isinstance_of_string(b"  "), True)
+        assert_equal( isinstance_of_string(b"asdasdsa"), True)
+        
+        assert_not_equal( isinstance_of_string(123434), True)
         
